3396d7dfa4c947076f09e1bad132ae1f9f15bb99
unknown <tungtt@vietmoz.edu.vn>
2018-11-21 22:41:44 +0700

    phuc test

---
+++ js/js.js
@@ -0,0 +1,70 @@
+$('#slider1').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        561:{
+            items:4
+        },
+        767:{
+            items:4
+        },
+        992:{
+            items:8
+        },
+        1000:{
+            items:8
+        }
+    }
+})
+
+$('#slider2').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        561:{
+            items:4
+        },
+        767:{
+            items:4
+        },
+        992:{
+            items:8
+        },
+        1000:{
+            items:8
+        }
+    }
+})
+$('#slider3').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        768:{
+            items:2
+        },
+        991:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+
+
